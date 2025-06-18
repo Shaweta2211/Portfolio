860223af290c4c4fd21e5d21e8ebcde345d04a08
Shaweta Sharma <shawetapan2211@gmail.com>
2025-06-19 00:33:38 +0530

    updated app

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,61 @@
+// import Navbar from "./components/Navbar";
+// import { Home } from "./Pages/Home";
+
+// function App() {
+//   return (
+//     <>
+//     <Navbar/>
+    
+//       <Home />
+//     </>
+//   );
+// }
+
+// export default App;
+
+
+
+
+import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import { Home } from "./Pages/Home";
+import { About } from "./components/About";
+import { Skills } from "./components/Skills";
+import { Experience } from "./components/Experience";
+import { Certificates } from "./components/Certficates";
+import Contact from "./components/Contact";
 
-function App() {
+const App = () => {
+  const [activeComponent, setActiveComponent] = useState("about");
+
+  const renderComponent = () => {
+    switch (activeComponent) {
+      case "home":
+        return<Home />;
+      case "about":
+        return <About />;
+      case "skills":
+        return <Skills />;
+      case "experience":
+        return <Experience />;
+      case "certificate":
+        return <Certificates />;
+      case "contact":
+        return <Contact />;
+      default:
+        return <About />;
+    }
+  };
+
   return (
-    <>
-    <Navbar/>
-    
-      <Home />
-    </>
+    <div>
+      <Navbar setActiveComponent={setActiveComponent} />
+      <div className="content">
+        {renderComponent()}
+      </div>
+    </div>
   );
-}
+};
 
 export default App;
-
-
-
 
